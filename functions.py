7ca8521f75472da8c37fbd37703b54157427b765
Andrea_Graziosi <andreaggraziosi@gmail.com>
2020-09-06 19:53:55 -0700

    Q4 def
    added def= which tells python we are creating a function, added descriptive parameters, and finished the code block, named the variable and added the result wich tells python to return the result

--- functions.py
+++ functions.py
@@ -6,4 +6,9 @@ print (result)
 result = area(14,2)
 print (result)
 result = area(50,3)
-#print (result)
+print (result)
+def subtract(current_year, model_year):
+     how_old_is_mycar = current_year - model_year
+     return how_old_is_mycar
+
+
