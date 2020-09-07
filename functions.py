db8c24f0b2ca146e887ca240cb6ad6b3babfcc5a
Andrea_Graziosi <andreaggraziosi@gmail.com>
2020-09-06 21:06:26 -0700

    Q6
    added a function def called divide() and printed the results

--- functions.py
+++ functions.py
@@ -14,6 +14,17 @@ result = subtract(2020,2005)
 print(result)
 result = subtract(2020,1998)
 print(result)
+#"Write a function called divide() which takes in two numbers and returns the first number divided by the second number"
+#To write this function definition we first need to identify what the inputs or parameters are and then what the output should be or what the function should return.
+#Q6
+def divide(number_of_cookies,number_of_guests):
+    cookies_per_guest = number_of_cookies / number_of_guests
+    return cookies_per_guest
+result = divide(30,5)
+print(result)
+
+
+
 
 
 
