225c4dc6f3fb785c57163b3877a40f3439fbfb51
Andrea_Graziosi <andreaggraziosi@gmail.com>
2020-09-06 19:34:49 -0700

    added Q3 from the Tutorial

--- functions.py
+++ functions.py
@@ -2,4 +2,8 @@ def area(width, height):
     result = width * height
     return result
 result = area(5, 6)
-#print (result)
+print (result)
+result = area(14,2)
+print (result)
+result = area(50,3)
+print (result)
